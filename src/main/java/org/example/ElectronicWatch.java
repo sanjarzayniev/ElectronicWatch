222538bb15bbe69e08a6fbf3aa4417c0322d2abd
Sanjar Zayniev <116029265+sanjarzayniev@users.noreply.github.com>
2023-10-24 22:06:07 +0500

    Update ElectronicWatch.java

--- src/main/java/org/example/ElectronicWatch.java
+++ src/main/java/org/example/ElectronicWatch.java
@@ -1,25 +1,24 @@
-package org.example;
-
 import java.util.Scanner;
 
 public class ElectronicWatch {
-    // public static void putZeroForMinute(int minutes) {
-    //     if (minutes <= 9 && minutes >= 0) {
-    //         System.out.println("0");
-    //     } else {
-    //         System.out.println("");
-    //     }
-    // }
+    public static String putZeroForMinute(int minutes) {
+        if (minutes <= 9 && minutes >= 0) {
+            return "0";
+        } else {
+            return "";
+        }
+    }
 
-    // public static void putZeroForSecond(int seconds) {
-    //     if (seconds <= 9 && seconds >= 0) {
-    //         System.out.println("0");
-    //     } else {
-    //         System.out.println("");
-    //     }
-    // }
+    public static String putZeroForSecond(int seconds) {
+        if (seconds <= 9 && seconds >= 0) {
+            return "0";
+        } else {
+            return "";
+        }
+    }
 
     public static void main(String[] args) {
+        System.out.print("Input the total seconds : ");
         Scanner scanner = new Scanner(System.in);
         int totalSeconds = scanner.nextInt();
         int hours = totalSeconds / 3600;
@@ -28,10 +27,10 @@ public class ElectronicWatch {
         int days = hours / 24;
         if (hours >= 24) {
 
-            hours = hours - 24*days; 
+            hours = hours - 24*days;
         }
 
-        System.out.println(hours + ":" + minutes + ":" + seconds);
+        System.out.println(hours + ":" +putZeroForMinute(minutes)+ minutes + ":" +putZeroForSecond(seconds)+ seconds);
         scanner.close();
     }
 }
